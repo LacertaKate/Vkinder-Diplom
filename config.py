c7c4b86cfb46b8dd14059b5e3f9449c561435620
LacertaKate <112935023+LacertaKate@users.noreply.github.com>
2022-12-17 12:00:22 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+user_token = ' ' # токен пользователя
+comm_token = ' ' # токен сообщества
+
+offset = 0
+line = range(0, 1000)
+
+host = '127.0.0.1'
+user = 'lackate'
+password = 'vjcrdf1010'
+db_name = 'diplom'
